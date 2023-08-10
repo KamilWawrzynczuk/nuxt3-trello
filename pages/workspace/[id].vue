baf5746554ebaeb934d0fc2ed9bcfd5e17992826
Kamil Wawrzynczuk <wawrzynczuk@mogic.com>
2023-08-10 12:02:14 +0200

    feature: ability to open workspaces

--- pages/workspace/[id].vue
+++ pages/workspace/[id].vue
@@ -7,13 +7,20 @@ import { useRoute } from 'vue-router'
 import { workspaceList } from '../../store/global.js'
 const route = useRoute();
 
-console.log(workspaceList.value)
+let workspaceName = ''
+
+
+onMounted(()=>{
+  workspaceName = workspaceList.value.filter(workspace => {
+    return workspace.id === Number(route.params.id);
+  })
+})
 </script>
 
 
 <template>
 
-  <h1>Workspace {{ route.params.id }}</h1>
+  <h1>{{ workspaceName[0]?.name }} workspace {{ route.params.id }}</h1>
 
 </template>
 
